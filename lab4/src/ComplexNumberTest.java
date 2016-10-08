6616ce281197b9bbada7556963c28d4a921a1628
glouzong <vadimshipko@gmail.com>
2016-10-08 19:00:19 +0300

    Поправил 4 лабу

--- lab4/src/ComplexNumberTest.java
+++ lab4/src/ComplexNumberTest.java
@@ -1,13 +1,13 @@
 public class ComplexNumberTest {
     public static void main(String[] args) {
-        ComlexNumber a = new ComlexNumber();
+        ComlexNumber a = new ComlexNumber(1.0, 2.0);
         ComlexNumber b = new ComlexNumber(3.0, 4.0);
         System.out.println(b.printAlgebraicForm());
         System.out.println(b.getExponentialForm());
         System.out.println(b.getTrigonometricForm());
         a = a.subtraction(b);
         System.out.println(a.printAlgebraicForm());
-        a.add(b);
+        a = a.add(b);
         System.out.println(a.printAlgebraicForm());
         a = a.divide(b);
         System.out.println(a.printAlgebraicForm());
